844d903bce6bad262aa76d808221a62030b7180d
kalmansour <63953899+kalmansour@users.noreply.github.com>
2022-04-11 18:07:46 +0300

    Restaurant model created with relevant fields, registered model into admin site and created random object

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,13 @@
+from unicodedata import name
 from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length = 120)
+    description = models.TextField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
